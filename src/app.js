3be30dc1ff2542f13a11232e47643d6ce59ed434
João Gustavo  Bispo <jgbispo20@gmail.com>
2021-05-18 16:46:24 -0300

    Remove

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 require("dotenv").config();
 const cors = require("cors");
 const routes = require("./router");
-const morgan = require("morgan");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
 app.use(routes);
 
 module.exports = app;
